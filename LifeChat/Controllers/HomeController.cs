5e8f74c6855ffc23beeccfd34ce186b05a5aeffd
Albion Berisha <albion.b@gjirafa.com>
2023-02-04 22:10:49 +0100

    Add project files.

---
+++ LifeChat/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
